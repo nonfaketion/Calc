6d40ca2e4464ce74b27b5e60cd6b1ad0a1cd93e7
nonfaketion <junming46@gmail.com>
2020-07-08 07:07:44 -0700

    Add buttons

---
+++ main.js
@@ -0,0 +1,28 @@
+function add(num1,num2){
+    return num1 + num2;
+}
+
+function subtract(num1,num2){
+    return num1 - num2;
+}
+function multiply(num1,num2){
+    return num1 * num2;
+}
+function divide(num1,num2){
+    return num1/num2;
+}
+
+function operate(num1, num2, operator) {
+    if (operator == "+") {
+        add(num1,num2);
+    }
+    else if (operator == "-") {
+        subtract(num1,num2);
+    }
+    else if (operator == "*") {
+        multiply(num1,num2);
+    }
+    else if (operator == "/") {
+        divide(num1,num2);
+    }
+}
